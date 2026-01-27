ecb8617a4df121d1c27ea6f53c440b041d87f8ea
Dan Lew <daniel@mightyacorn.com>
2026-01-27 12:58:58 -0600

    Reveal temporary chats in the sidebar
    Since NJ is doing temporary chats only, we want a way for users to
    return to temporary chats (though eventually they will disappear).
    
    To make them appear, we had to do three things:
    
    1. Stop filtering temp chats from API calls that return all convos.
    
    2. Hook up temporary chats to title generation.
    
    3. Hook up temporary chats to dynamically updating convo list.

--- api/models/Conversation.js
+++ api/models/Conversation.js
@@ -178,7 +178,9 @@ module.exports = {
       filters.push({ tags: { $in: tags } });
     }
 
+    /* NJ: Show temporary chats in the sidebar
     filters.push({ $or: [{ expiredAt: null }, { expiredAt: { $exists: false } }] });
+     */
 
     if (search) {
       try {
@@ -278,7 +280,9 @@ module.exports = {
       const results = await Conversation.find({
         user,
         conversationId: { $in: conversationIds },
+        /* NJ: Show temporary chats in the sidebar
         $or: [{ expiredAt: { $exists: false } }, { expiredAt: null }],
+         */
       }).lean();
 
       results.sort((a, b) => new Date(b.updatedAt) - new Date(a.updatedAt));

02224b9d44c0bdb01b10bd65e448610000028a8c
Josh Fink <josh.fink@innovation.nj.gov>
2026-04-20 16:09:05 -0500

    fix bug when rds port is optional

--- nj/infra/lib/ecs-stack.ts
+++ nj/infra/lib/ecs-stack.ts
@@ -297,7 +297,8 @@ export class EcsStack extends cdk.Stack {
 
     // Allow RAG API to connect to RDS
     if (props.rdsSecurityGroup && props.rdsPort) {
-      ragApiService.connections.allowTo(props.rdsSecurityGroup, ec2.Port.tcp(parseInt(props.rdsPort)), "RAG API to RDS");
+      const rdsPortNumber = parseInt(props.rdsPort, 10);
+      ragApiService.connections.allowTo(props.rdsSecurityGroup, ec2.Port.tcp(rdsPortNumber), "RAG API to RDS");
     }
 
     // Allow LibreChat to connect to RAG API
@@ -305,7 +306,8 @@ export class EcsStack extends cdk.Stack {
 
     // Allow LibreChat to connect to RDS
     if (props.rdsSecurityGroup && props.rdsPort) {
-      librechatService.service.connections.allowTo(props.rdsSecurityGroup, ec2.Port.tcp(parseInt(props.rdsPort)), "LibreChat to RDS");
+      const rdsPortNumber = parseInt(props.rdsPort, 10);
+      librechatService.service.connections.allowTo(props.rdsSecurityGroup, ec2.Port.tcp(rdsPortNumber), "LibreChat to RDS");
     }
 
     new cdk.CfnOutput(this, "MongoImageUri", { value: props.mongoImage });
